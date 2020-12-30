545fb5791d1177802816abdd33cafefd10d0a25c
Zhian N. Kamvar <zkamvar@gmail.com>
2020-12-30 12:00:44 -0800

    try to fix things

--- check-pr.sh
+++ check-pr.sh
@@ -6,17 +6,13 @@ repo=${2:?"repository required"}
 URI="https://api.github.com/repos/${repo}/pulls/${NR}"
 ACCEPT="Accept:application/vnd.github.v3+json"
 
-echo "URI: ${URI}"
 
 MERGE_STAT=$(curl -sIH "${ACCEPT}" "${URI}/merge")
 FILES=$(curl -sH "${ACCEPT}" "${URI}/files")
 
-echo "MERGE HEADER: ${MERGE_STAT}"
-echo "FILE RESPONSE: ${FILES}"
-
-NO_FILES=$(echo ${FILES} | grep -c '"message": "Not Found"')
-GH_FILES=$(echo ${FILES} | grep -c '"filename": ".github')
-MERGED=$(echo ${MERGE_STAT} | grep -c 'status: 202')
+NO_FILES=$(grep -c "\"message\": \"Not Found\"" <<< ${FILES})
+GH_FILES=$(grep -c "\"filename\": \".github" <<< ${FILES})
+MERGED=$(grep -c "status: 202" <<< ${MERGED_STAT})
 
 if [ ${NO_FILES} -ne 0 ] || [ ${GH_FILES} -ne 0 ] || [ ${MERGED} -ne 0 ]; then
   echo "Invalid PR Number (${NR}):\n"
