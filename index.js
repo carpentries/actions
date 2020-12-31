c79d82109540e8a0c8bc77accafb8d7f62ab8ae5
Zhian N. Kamvar <zkamvar@gmail.com>
2020-12-30 16:36:39 -0800

    add github repository var

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ try {
   const pullRequest = core.getInput('pr');
   const repository  = core.getInput('repo');
   console.log(`Hello ${repository}/${pullRequest}!`);
+  console.log(`Hello ${github.repository}?`)
   const valid = github.repository == repository;
   core.setOutput("VALID", valid);
   // Get the JSON webhook payload for the event that triggered the workflow
