fd6eb990d94bad8f5760b07514814fb30742f352
Zhian N. Kamvar <zkamvar@gmail.com>
2020-12-30 17:38:25 -0800

    oops

--- index.js
+++ index.js
@@ -36,29 +36,18 @@ async function run() {
     pull_number: Number(PR),
   });
   
+  const files = pullRequestFiles.map(getFilename).catch(error) { console.log('oops') };
+  const valid = files.reduce(notAction, true).catch(error) { console.log('oops') };
 
   console.log(`Has Merged: ${pullRequestMerged}`);
-  console.log(`Files: ${pullRequestFiles.map(getFilename)}`);
-  console.log(`Any GitHub: ${pullRequestFiles.map(getFilename).reduce(notAction, true)}`);
+  console.log(`Files: ${files}`);
+  console.log(`Any GitHub: ${valid}`);
+  core.setOutput("VALID", valid);
 }
 
 
 try {
   run();
 } catch(error) {
-  core.setFailed(error.message);
-}
-try {
-  // `who-to-greet` input defined in action metadata file
-  const pullRequest = core.getInput('pr');
-  const repository  = core.getInput('repo');
-  console.log(`Hello ${repository}/${pullRequest}!`);
-  console.log(`Hello ${github.context.payload.repository.pulls_url}?`)
-  const valid = github.repository == repository;
-  core.setOutput("VALID", valid);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  // console.log(`The event payload: ${payload}`);
-} catch (error) {
   core.setFailed(error.message);
 }
