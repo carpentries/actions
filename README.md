97d58b2da9be2e0d762d5be944185a9b135f49fa
Zhian N. Kamvar <zkamvar@gmail.com>
2020-12-30 16:32:00 -0800

    first javascript version

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# check-valid-pr
+# Hello world javascript action
+
+This action prints "Hello World" or "Hello" + the name of a person to greet to the log.
+
+## Inputs
+
+### `pr`
+
+**Required** The pull request number
+
+### `repo`
+
+**Required** The value of the repository. Defaults to `github.repository`
+
+## Outputs
+
+### `VALID`
+
+Tells you if the Pull Request is valid (e.g. it exists and does not modify any actions files)
+
+## Example usage
+
+uses: zkamvar/check-pr@use-js
+with:
+  pr: 227
+  repo: grunwaldlab/poppr
---
+++ index.js
@@ -0,0 +1,16 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+  // `who-to-greet` input defined in action metadata file
+  const pullRequest = core.getInput('pr');
+  const repository  = core.getInput('repo');
+  console.log(`Hello ${repository}/${pullRequest}!`);
+  const valid = github.repository == repository;
+  core.setOutput("VALID", valid);
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
+} catch (error) {
+  core.setFailed(error.message);
+}
