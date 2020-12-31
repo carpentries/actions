18a029938ccf3fe74da33f72e57eb366c4db9549
Zhian N. Kamvar <zkamvar@gmail.com>
2020-12-30 16:49:52 -0800

    trying out a run

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+async function run() {
+  // This should be a token with access to your repository scoped in as a secret.
+  // The YML workflow will need to set myToken with the GitHub Secret Token
+  // myToken: ${{ secrets.GITHUB_TOKEN }}
+  // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
+  const myToken    = core.getInput('token');
+  const PR         = core.getInput('pr');
+  const repository = core.getInput('repo').split('/');
+
+  const octokit = github.getOctokit(myToken)
+
+  // You can also pass in additional options as a second parameter to getOctokit
+  // const octokit = github.getOctokit(myToken, {userAgent: "MyActionVersion1"});
+
+  const { data: pullRequest } = await octokit.pulls.get({
+    owner: repository[0],
+    repo: repository[1],
+    pull_number: Int(PR),
+  });
+
+  console.log(pullRequest);
+}
+
+
 try {
+  run()
   // `who-to-greet` input defined in action metadata file
   const pullRequest = core.getInput('pr');
   const repository  = core.getInput('repo');
