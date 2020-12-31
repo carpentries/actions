5d2edadb8f21b9095786eb8b7109c53b57b56b20
Zhian N. Kamvar <zkamvar@gmail.com>
2020-12-30 18:12:03 -0800

    try handling those errors

--- index.js
+++ index.js
@@ -28,13 +28,13 @@ async function run() {
     owner: repository[0],
     repo: repository[1],
     pull_number: Number(PR),
-  }).catch(err);
+  }).catch(err => { console.log(err) });
 
   const { data: pullRequestFiles } = await octokit.pulls.listFiles({
     owner: repository[0],
     repo: repository[1],
     pull_number: Number(PR),
-  }).catch(err);
+  }).catch(err => { console.log(err) } );
   
   const files = pullRequestFiles.map(getFilename);
   const valid = files.reduce(notAction, true);
