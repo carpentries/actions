cf94c15827d791fb0b4a5d8758591fc3e7bec6b8
Zhian N. Kamvar <zkamvar@gmail.com>
2021-01-07 11:32:09 -0800

    parameterize better

--- comment-diff/index.js
+++ comment-diff/index.js
@@ -18,7 +18,7 @@ async function run() {
   // var body = String(fs.readFileSync('./diff.md', {encoding:'utf8', flag:'r'}));
   var page = 0;
   var myBot = -1;
-  var issue_comments = await octokit.issues.listComments({
+  var { data: issue_comments } = await octokit.issues.listComments({
       owner: repository[0],
       repo: repository[1],
       issue_number: Number(PR),
@@ -32,27 +32,28 @@ async function run() {
     });
   var bots;
 
+
+  do {
+    bots = issue_comments.map(item => item.type === "bot" && item.user.login == "github-actions[bot]");
+    myBot = bots.indexOf(true);
+    { data: issue_comments } = await octokit.issues.listComments({
+      owner: repository[0],
+      repo: repository[1],
+      issue_number: Number(PR),
+      page: page
+
+    }).catch(err => { 
+      // HTTP errors turn into a failed run --------------------------------------
+      console.log(err);
+      core.setFailed(`There was a problem with the request (Status ${err.status}). See log.`);
+      process.exit(1);
+    });
+    page++;
+  }
+  while(myBot <= 0 && issue_comments.length > 0);
+
   console.log(issue_comments);
-  console.log(JSON.stringify(issue_comments));
-
-  // do {
-  //   bots = issue_comments.map(item => item.type === "bot" && item.user.login == "github-actions[bot]");
-  //   myBot = bots.indexOf(true);
-  //   issue_comments = await octokit.issues.listComments({
-  //     owner: repository[0],
-  //     repo: repository[1],
-  //     issue_number: Number(PR),
-  //     page: page
-
-  //   }).catch(err => { 
-  //     // HTTP errors turn into a failed run --------------------------------------
-  //     console.log(err);
-  //     core.setFailed(`There was a problem with the request (Status ${err.status}). See log.`);
-  //     process.exit(1);
-  //   });
-  //   page++;
-  // }
-  // while(myBot <= 0 || issue_comments.length == 0);
+  console.log(myBot);
 
   // if (myBot > 0) {
   //   var id = await octokit.issues.updateComment({
