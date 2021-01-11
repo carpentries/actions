a5df30c32fb0c63a218c42b39231f13b2fbe0c2e
Zhian N. Kamvar <zkamvar@gmail.com>
2021-01-11 10:11:01 -0800

    add remove-branch action

---
+++ remove-branch/README.md
@@ -0,0 +1,60 @@
+# Remove branch created via PR
+
+The workflow in the main README shows that a staging branch is created from the PR. The branch name is `{prefix}-{pr}` (which at the moment is `md-outputs-PR-{pr}`) so that it's unique and tied to the PR. This branch is only relevant for the life of the PR, so it needs to be deleted once the PR is closed to avoid cluttering the repository. 
+
+## Inputs
+
+### `prefix`
+
+**Required** The prefix of the branch. Defaults to `md-outputs-PR`
+
+### `pr`
+
+**Required** The pull request number
+
+### `repo`
+
+**Required** The value of the repository. Defaults to `github.repository`
+
+### `token`
+
+**Required** The default token for authorization. Defaults to `github.token`
+
+
+## Example usage
+
+This example assumes that you have an action on close that records the pull
+request number.
+
+```yaml
+name: "Remove Temporary Branch"
+
+on:
+  workflow_run:
+    workflows: ["Close Pull Request Signal"]
+    types:
+      - completed
+
+jobs:
+  delete:
+    name: "Delete branch from Pull Request"
+    runs-on: ubuntu-latest
+    if: >
+      ${{ github.event.workflow_run.event == 'pull_request' &&
+      github.event.workflow_run.conclusion == 'success' }}
+    steps:
+      - name: 'Download artifact'
+        uses: zkamvar/actions/download-workflow-artifact@main
+        with:
+          run: ${{ github.event.workflow_run.id }}
+          name: pr
+      - name: "Get PR Number"
+        id: get-pr
+        run: |
+          unzip pr.zip
+          echo "::set-output name=NUM::$(<./NUM)"
+      - name: 'Remove branch'
+        uses: zkamvar/actions/remove-branch@main
+        with:
+          pr: ${{ steps.get-pr.outputs.NUM }}
+```
---
+++ remove-branch/index.js
@@ -0,0 +1,50 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+async function run() {
+  // This should be a token with access to your repository scoped in as a secret.
+  // The YML workflow will need to set myToken with the GitHub Secret Token
+  // myToken: ${{ secrets.GITHUB_TOKEN }}
+  // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
+  const myToken    = core.getInput('token');
+  const prefix     = core.getInput('prefix');
+  const PR         = core.getInput('pr');
+  const repository = core.getInput('repo').split('/');
+  const octokit    = github.getOctokit(myToken)
+
+
+  const ref        = `heads/${prefix}-${pr}`
+
+  // Check if ref exists -------------------------------------------------------
+  const pullRequest = await octokit.git.getRef({
+    owner: repository[0],
+    repo: repository[1],
+    ref: ref
+  }).catch(err => { 
+    // HTTP errors turn into a failed run --------------------------------------
+    console.log(err);
+    core.setFailed(`There was a problem with the request (Status ${err.status}). See log.`);
+    process.exit(1);
+  });
+
+  // Remove ref ----------------------------------------------------------------
+  const id = await octokit.git.deleteRef({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    ref: ref
+  }).catch(err => { 
+    // HTTP errors turn into a failed run --------------------------------------
+    console.log(err);
+    core.setFailed(`There was a problem with the request (Status ${err.status}). See log.`);
+    process.exit(1);
+  });
+
+  console.log(`Deleted ${id}`)
+
+}
+
+try {
+  run();
+} catch(error) {
+  core.setFailed(error.message);
+}
