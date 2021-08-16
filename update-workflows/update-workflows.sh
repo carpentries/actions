ba9cc92c71b8f6c7806a002ebce7c548c2e745cf
Zhian N. Kamvar <zkamvar@gmail.com>
2021-08-16 15:40:21 -0700

    update cleaner

--- update-workflows/update-workflows.sh
+++ update-workflows/update-workflows.sh
@@ -4,18 +4,20 @@ set -eo pipefail
 # Download and update sandpaper workflow files from an upstream repository
 #
 # usage: 
-#   bash update-workflows.sh [UPSTREAM] [SOURCE]
+#   bash update-workflows.sh [UPSTREAM] [SOURCE] [CLEAN]
 #
 # args:
 #   UPSTREAM - a version number from which to fetch the workflows. By default,
 #     this is fetched from https://carpentries.r-universe.dev/packages/sandpaper
 #   SOURCE - a CRAN-like repository from which to fetch a tarball of sandpaper
 #     By default this is fetched from https://carpentries.r-universe.dev/
+#   CLEAN files to clean as a pattern. Example: *.yaml will clean all the yaml
+#     files, but will leave the yml files alone.
 #
 # example: Reset the workflow versions to that of 0.0.0.9041 from the drat
 #   archives
 #
-# bash update-workflows.sh 0.0.0.9041 https://carpentries.github.io/drat/
+# bash update-workflows.sh 0.0.0.9041 https://carpentries.github.io/drat/ *.yaml
 
 # Fail if we aren't in a sandpaper repository
 if [[ -r .github/workflows/sandpaper-main.yaml || -r .github/workflows/sandpaper-version.txt ]]; then
@@ -28,6 +30,7 @@ fi
 # Set variables needed
 UPSTREAM="${1:-}"
 SOURCE="${2:-https://carpentries.r-universe.dev}"
+CLEAN="${3:-}"
 CURRENT=$(cat .github/workflows/sandpaper-version.txt)
 
 # Fetch upstream version from the API if we don't have that information
@@ -54,6 +57,11 @@ echo "::endgroup::"
 
 # Copy the contents if the versions do not match
 if [[ ${CURRENT} != ${UPSTREAM} ]]; then
+  if [[ ${CLEAN} ]]; then
+    echo "::group::Cleaning all ${CLEAN} workflow files"
+    rm -fv .github/workflows/${CLEAN}
+    echo "::endgroup::"
+  fi
   echo "::group::Copying files and updating the version number"
   curl ${SOURCE}/src/contrib/sandpaper_${UPSTREAM}.tar.gz | \
     tar -C ${TMP} --wildcards -xzv sandpaper/inst/workflows/*
