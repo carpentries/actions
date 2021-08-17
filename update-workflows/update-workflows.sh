9cb9717c6f83531cdd225c69fd5e96ecc23d7d3a
Zhian N. Kamvar <zkamvar@gmail.com>
2021-08-16 17:01:16 -0700

    use drat if custom upstream

--- update-workflows/update-workflows.sh
+++ update-workflows/update-workflows.sh
@@ -19,8 +19,6 @@ set -eo pipefail
 #
 # bash update-workflows.sh 0.0.0.9041 https://carpentries.github.io/drat/ *.yaml
 
-pwd
-
 # Fail if we aren't in a sandpaper repository
 if [[ -r .github/workflows/sandpaper-main.yaml || -r .github/workflows/sandpaper-version.txt ]]; then
   echo "" > /dev/null
@@ -39,6 +37,8 @@ CURRENT=$(cat .github/workflows/sandpaper-version.txt)
 if [[ ${UPSTREAM} == 'current' ]]; then
   UPSTREAM=$(curl ${SOURCE}/packages/sandpaper/)
   UPSTREAM=$(echo ${UPSTREAM} | grep '[.]' | sed -E -e 's/[^0-9.]//g')
+elif [[ ${SOURCE} == 'https://carpentries.r-universe.dev' ]]; then
+  SOURCE=https://carpentries.github.io/drat
 fi
 
 # Create a temporary directory for the sandpaper resource files to land in
@@ -71,8 +71,8 @@ if [[ ${CURRENT} != ${UPSTREAM} ]]; then
   echo "::endgroup::"
   NEEDS_UPDATE=$(git status --porcelain)
   if [[ ${NEEDS_UPDATE} ]]; then
-    echo "::set-output name=old::$(echo ${CURRENT})"
-    echo "::set-output name=new::$(echo ${UPSTREAM})"
+    echo "::set-output name=old::${CURRENT}"
+    echo "::set-output name=new::${UPSTREAM}"
     echo "Updating version number to ${UPSTREAM}"
     echo ${UPSTREAM} > .github/workflows/sandpaper-version.txt
   else
