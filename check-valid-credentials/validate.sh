d4efd15e65998683635807da8a65935ddd3e7d72
Zhian N. Kamvar <zkamvar@gmail.com>
2021-08-25 11:50:36 -0700

    add script to check a token's credentials (#21)
    * add script to check a token's credentials
    
    This will hopefully avoid cryptic error messages from downstream actions
    
    * add token test
    
    * fix test
    
    * oh god THAT's what I named the branch?
    
    * see what's happening
    
    * wat
    
    * echo and rename
    
    * check something out
    
    * remove debug
    
    * fix workflow file
    
    * fix step
    
    * one step
    
    * see what this is
    
    * use the correct output
    
    * trace again
    
    * duh GH token has no repo scope for PRs

---
+++ check-valid-credentials/validate.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+# Download and update sandpaper workflow files from an upstream repository
+#
+# usage: 
+#   bash validate.sh GITHUB_PAT
+#
+# args:
+#   GITHUB_PAT a github personal access token. This is only ever used for 
+#   accessing the headers of the /user/ endpoint of the github API
+#
+# Fail if we aren't in a sandpaper repository
+
+PAT=${1:-}
+
+# Create a temporary directory for the sandpaper resource files to land in
+if [[ -d ${TMPDIR} ]]; then
+  TMP="${TMPDIR}/sandpaper-${RANDOM}"
+elif [[ -d /tmp/ ]]; then
+  TMP="/tmp/sandpaper-${RANDOM}"
+else
+  TMP="../sandpaper-${RANDOM}"
+fi
+mkdir -p ${TMP}
+
+if [[ ${PAT} ]]
+then
+
+  headerfile=${TMP}/${RANDOM}
+  curl --dump-header ${headerfile} --head -H "Authorization: token ${PAT}" \
+    https://api.github.com/user/ > /dev/null
+  WORKFLOW=$(grep -ic 'x-accepted-oauth-scopes: .*workflow' ${headerfile} || echo '0')
+  REPO=$(grep -ic 'x-accepted-oauth-scopes: .*repo' ${headerfile} || echo '0')
+  rm -r ${TMP}
+
+  if [[ ${WORKFLOW} == 1 ]]
+  then
+    echo "::set-output name=wf::true"
+  fi
+
+  if [[ ${REPO} == 1 ]]
+  then
+    echo "::set-output name=repo::true"
+  fi
+
+else
+  echo "Nothing to do!"
+fi
