692e9cde7efb7dd38ed531050512cbea3aaa36f7
Zhian N. Kamvar <zkamvar@gmail.com>
2021-09-21 10:46:54 -0700

    format arrows

--- check-valid-pr/index.js
+++ check-valid-pr/index.js
@@ -63,7 +63,7 @@ async function run() {
         let invalid_files = files.filter(e => !isNotAction(e));
         let vf = valid_files.join(", ");
         let inv = invalid_files.join(", ");
-        core.setFailed(`PR #${PR} contains a mix of workflow files and regular files. This could be malicious.\n regular files: ${vf}\nworkflow files: ${inv}`)
+        core.setFailed(`PR #${PR} contains a mix of workflow files and regular files. This could be malicious.\n-> regular files: ${vf}\n->workflow files: ${inv}`)
       }
       console.log(`Files in PR: ${files}`);
     } else {
