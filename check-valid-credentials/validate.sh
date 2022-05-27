d9fcc96abf78300f08b75f6e5107e8304686e528
Zhian N. Kamvar <zkamvar@gmail.com>
2022-05-27 09:05:11 -0700

    attempt to add github step summary

--- check-valid-credentials/validate.sh
+++ check-valid-credentials/validate.sh
@@ -38,6 +38,15 @@ then
   if [[ ${WORKFLOW} == 1 ]]
   then
     echo "::set-output name=wf::true"
+  else
+    echo "The \`SANDPAPER_WORKFLOW\` secret is missing, invalid," >> $GITHUB_STEP_SUMMARY
+    echo "or does not have the right scope to update the package cache." >> $GITHUB_STEP_SUMMARY
+    echo "If you want to have automated pull request updates to your package cache," >> $GITHUB_STEP_SUMMARY
+    echo "you will need to generate a new token by visiting " >> $GITHUB_STEP_SUMMARY
+    echo "https://github.com/settings/tokens/new?scopes=repo,workflow&description=Sandpaper%%20Token%%20%%28${{ github.repository }}%%29\n" >> $GITHUB_STEP_SUMMARY
+    echo "Once you have created the token, copy it to your clipboard and go to" >> $GITHUB_STEP_SUMMARY
+    echo "https://github.com/${{ github.repository }}/settings/secrets/actions/new" >> $GITHUB_STEP_SUMMARY
+    echo "and enter SANDPAPER_WORKFLOW for the 'Name' and paste your key for the 'Value'." >> $GITHUB_STEP_SUMMARY
   fi
 
   if [[ ${REPO} == 1 ]]
