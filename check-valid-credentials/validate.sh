2813abc6e26b817206552577b796012af4ee67d1
Zhian N. Kamvar <zkamvar@gmail.com>
2022-05-27 10:04:45 -0700

    edit step 1 text

--- check-valid-credentials/validate.sh
+++ check-valid-credentials/validate.sh
@@ -24,9 +24,9 @@ else
 fi
 mkdir -p ${TMP}
 
+# Set up output first because there
 TOKEN_NAME="Sandpaper%20Token%20%28${GITHUB_REPOSITORY}%29"
 TOKEN_URL="https://github.com/settings/tokens/new?scopes=repo,workflow&description=${TOKEN_NAME}"
-
 echo "## :warning: Missing Token" >> $GITHUB_STEP_SUMMARY
 echo "" >> $GITHUB_STEP_SUMMARY
 echo "The \`SANDPAPER_WORKFLOW\` secret is missing, invalid, or does not" \
@@ -37,7 +37,7 @@ echo "If you want to have automated pull request updates to your package cache,"
 echo "" >> $GITHUB_STEP_SUMMARY
 echo "### Steps to Generate a New Token" >> $GITHUB_STEP_SUMMARY
 echo "" >> $GITHUB_STEP_SUMMARY
-echo "1. :key: [Click here to generate a new token](${TOKEN_URL}) called 'Sandpaper Token (${GITHUB_REPOSITORY})' from your GitHub Account" >> $GITHUB_STEP_SUMMARY
+echo "1. :key: [Click here to generate a new token](${TOKEN_URL}) called 'Sandpaper Token (${GITHUB_REPOSITORY})' with the "repo" and "workflow" scopes from your GitHub Account" >> $GITHUB_STEP_SUMMARY
 echo "2. :clipboard: Copy your new token to your clipboard" >> $GITHUB_STEP_SUMMARY
 echo "3. Go To https://github.com/${GITHUB_REPOSITORY}/settings/secrets/actions/new" >> $GITHUB_STEP_SUMMARY
 echo "   - enter \`SANDPAPER_WORKFLOW\` for the 'Name'" >> $GITHUB_STEP_SUMMARY
