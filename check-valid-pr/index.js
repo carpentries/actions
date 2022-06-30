61724ede5ca3e6674d2f71771b215d27e4f2997e
Zhian N. Kamvar <zkamvar@gmail.com>
2022-06-30 07:41:54 -0700

    send to log

--- check-valid-pr/index.js
+++ check-valid-pr/index.js
@@ -66,13 +66,14 @@ async function run() {
       if (!valid) {
         PR_msg = `## :danger: DANGER :danger:
 
-//         the fork ${pullRequest.data.user.login}/${repository[1]} has divergent
-//         history and contains an invalid commit (${bad_origin}) from the former
-//         version of this repository before the switch to The Workbench. 
+        the fork ${pullRequest.data.user.login}/${repository[1]} has divergent
+        history and contains an invalid commit (${bad_origin}) from the former
+        version of this repository before the switch to The Workbench. 
 
-//         @${pullRequest.data.user.login}, if you want to contribute your changes,
-//         you must [delete your fork](https://docs.github.com/en/repositories/creating-and-managing-repositories/deleting-a-repository) and re-fork this repository.
-//         `;
+        @${pullRequest.data.user.login}, if you want to contribute your changes,
+        you must [delete your fork](https://docs.github.com/en/repositories/creating-and-managing-repositories/deleting-a-repository) and re-fork this repository.
+        `;
+        console.log(PR_msg);
         core.setOutput("MSG", PR_msg);
       }
 
